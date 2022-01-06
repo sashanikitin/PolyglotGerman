7563f64f52d8e90096efdbc73edb8d8b0b6b9695
Oleksandr Nikitin <supersashanikitin@gmail.com>
2022-01-06 19:22:41 +0200

    Second for Ann

--- lib/UI/main_page.dart
+++ lib/UI/main_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 import 'first_page.dart';
@@ -6,39 +9,97 @@ class MainRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final title = 'Learning German';
+
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text(title)),
       ),
-      body:ListView(
-          children: <Widget>[
-        Container(
-        height: 100.0,
-        child:
-            ListTile(
-              leading: Icon(Icons.map),
-              title:  ElevatedButton(
-                child: Text('First Lesson'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => FirstPage(title: 'First Lesson')),
-                  );
-                },
-              ),
-            )),
-            ListTile(
-              leading: Icon(Icons.photo_album),
-              title: Text('Album'),
-            ),
-            ListTile(
-              leading: Icon(Icons.phone),
-              title: Text('Phone'),
-            ),
-          ],
-        ),
-      );
-
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              flex: 6,
+              child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                FirstPage(title: 'Learning basic')),
+                      );
+                    },
+                    child: AutoSizeText(
+                      "Learning Basic",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.black,
+                        // fixedSize: const Size(300, 100),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                  ))),
+          /////////////////////////////////////////////////////////////////////////////////////////////////
+          Expanded(
+              flex: 1,
+              child:SizedBox()
+          ),
+          /////////////////////////////////////////////////////////////////////////////////////////////////
+          Expanded(
+              flex: 6,
+              child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                FirstPage(title: 'Learning words')),
+                      );
+                    },
+                    child: AutoSizeText(
+                      "Learning words",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        // fixedSize: const Size(300, 100),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                  ))),
+          ////////////////////////////////////////////////////////////////////////////////////////////////
+      Expanded(
+        flex: 1,
+        child:SizedBox()
+      ),
+          /////////////////////////////////////////////////////////////////////////////////////////////////
+          Expanded(
+              flex: 6,
+              child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                FirstPage(title: 'Settings')),
+                      );
+                    },
+                    child: AutoSizeText(
+                      "Settings",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.yellow,
+                        // fixedSize: const Size(300, 100),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                  ))),
+          ////////////////////////////////////////////////////////////////////////////////////////////////
+        ],
+      ),
+    );
   }
 }
-
